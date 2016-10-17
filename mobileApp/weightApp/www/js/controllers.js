655c4905c45a8276076123c3d07dbe11f1902ce4
eyal <eyalfr2@gmail.com>
2016-10-17 16:25:16 +0300

    another google map fix

--- mobileApp/weightApp/www/js/controllers.js
+++ mobileApp/weightApp/www/js/controllers.js
@@ -595,18 +595,13 @@ angular.module('weightapp.controllers', ['weightapp.factory'])
      * should define div with id="map" and give it height,
      * sometimes the background of body Causing problems.   
      */   	
-	$scope.selectWeight = function(index) {
-		setTimeout(function() {
-			//$scope.loading = true;
-			var selectWeightId = $scope.myWeights[index];
-			if (selectWeightId) {
-				var latitude = selectWeightId.latitude;
-				var longitude = selectWeightId.longitude;
-				$scope.mapPopup(latitude, longitude);
-				//$scope.loading = false;
-				//$state.go('app.task');
-			}
-		}, 0);
+	$scope.selectWeight = function(index) {					
+		var selectWeightId = $scope.myWeights[index];
+		if (selectWeightId) {
+			var latitude = selectWeightId.latitude;
+			var longitude = selectWeightId.longitude;
+			$scope.mapPopup(latitude, longitude);				
+		}
 	};
 
 	$scope.mapPopup = function(latitude, longitude) {
@@ -620,34 +615,30 @@ angular.module('weightapp.controllers', ['weightapp.factory'])
 		});
 		mapPopup.then(function(res) {
 			// console.log('Thank you for not eating my delicious ice cream cone');					
-			document.getElementById('mapParent').appendChild(document.getElementById('map'));
-			document.getElementById("map").innerHTML = '';			
 		});
 		document.getElementsByClassName("popup")[0].style.maxHeight = '100%';
 		document.getElementsByClassName("popup")[0].style.height = '400px';
 		document.getElementsByClassName("popup")[0].style.maxWidth = '100%';
 		document.getElementsByClassName("popup")[0].style.width = '300px';
 		document.getElementsByClassName("popup-body")[0].style.height = '100%';
-
-		var mapOptions = {
-			center: new google.maps.LatLng(0, 0),
-			zoom: 1,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-		map = new google.maps.Map(document.getElementById("map"), mapOptions);
-		var latLong = new google.maps.LatLng(latitude, longitude);
-		var marker = new google.maps.Marker({
-			position: latLong
-		});
-		marker.setMap(map);
-		map.setZoom(8);
-		map.setCenter(marker.getPosition());
 		setTimeout(function() {
-			var newParent = document.getElementsByClassName("popup-body")[0];
-			var oldParent = document.getElementById('mapParent');
-			while (oldParent.childNodes.length > 0) {
-				newParent.appendChild(oldParent.childNodes[0]);
-			}
+			var mapDiv = document.createElement("div");
+			mapDiv.setAttribute("id", "map");
+			mapDiv.style.height = '100%';
+			document.getElementsByClassName("popup-body")[0].appendChild(mapDiv);
+			var mapOptions = {
+				center: new google.maps.LatLng(0, 0),
+				zoom: 1,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+			var latLong = new google.maps.LatLng(latitude, longitude);
+			var marker = new google.maps.Marker({
+				position: latLong
+			});
+			marker.setMap(map);
+			map.setZoom(8);
+			map.setCenter(marker.getPosition());
 		}, 0);
 	};	
 
